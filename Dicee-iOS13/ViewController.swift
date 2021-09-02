6478715628cfdfb78931cbb52d91cbfdd1b9a940
Cory Carte <7457451+corycarte@users.noreply.github.com>
2021-09-02 09:52:57 -0400

    Completed lesson 38. Responding to User Interactions with IBActions.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewL: UIImageView!
+    @IBOutlet weak var diceImageViewR: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewL.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewR.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Two 4's!")
+        diceImageViewL.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewR.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
